5a6fc3a2eddd44d72abbf7946bae615ce2eab894
Christopher Diep <diep.christopher@gmail.com>
2017-11-03 06:02:36 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -189,9 +189,9 @@ describe "#display_board in 'lib/display_board.rb" do
       rows = output.split("\n")
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" X | X | X ")
+      expect(rows[4]).to eq(" O | O | O ")
       expect(true).to be(true)
     end
   end
